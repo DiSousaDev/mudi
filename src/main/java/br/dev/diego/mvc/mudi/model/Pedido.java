3384b4a0da35c735aaba48790f99f89a282d32db
Diego Sousa <diegosous@gmail.com>
2021-01-23 01:21:07 -0300

    Criando PedidoRepository

--- src/main/java/br/dev/diego/mvc/mudi/model/Pedido.java
+++ src/main/java/br/dev/diego/mvc/mudi/model/Pedido.java
@@ -1,9 +1,18 @@
 package br.dev.diego.mvc.mudi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity(name = "tb_pedido")
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nomeProduto;
     private BigDecimal valorNegociado;
---
+++ src/main/java/br/dev/diego/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,10 @@
+package br.dev.diego.mvc.mudi.repository;
+
+import br.dev.diego.mvc.mudi.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+}
